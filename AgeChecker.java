e676a3aafb348e0d5c6e3c6f2ab57d123957f54c
Solvealgoritms22 <112198161+Solvealgoritms22@users.noreply.github.com>
2023-02-24 16:26:39 -0400

    Add files via upload

---
+++ AgeChecker.java
@@ -0,0 +1,16 @@
+//package agechecker;
+import java.util.Scanner;
+public class AgeChecker {
+public static void main(String[] args) {
+Scanner in = new Scanner(System.in);
+int age;
+System.out.print("Please enter your age: ");
+age = in.nextInt();
+if(age > 20)
+System.out.println("You are an adult");
+else
+System.out.println("You are not an adult");
+//endif
+in.close();
+}//end method main
+}//end class AgeChecker
