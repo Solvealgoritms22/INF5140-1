0ea85f4f35e758ca9b16b5065999be8e82827c35
Solvealgoritms22 <112198161+Solvealgoritms22@users.noreply.github.com>
2023-02-24 13:48:31 -0400

    Add files via upload

---
+++ StringOperations.java
@@ -0,0 +1,12 @@
+//package stringoperations;  <----- Mal uso de package, de este modo no deja compilar
+
+public class StringOperations {
+
+public static void main(String[] args){
+
+String str1 = "Hello";
+String str2 = "Duke";
+String str3; //uninitialized string
+
+  }//end method main
+}//end class StringOperations
